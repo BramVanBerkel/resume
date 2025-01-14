e8bff40827ec43131738cc62f7c1b01a4ab8fcfd
bramvanberkel <bramvanberkel.prive@gmail.com>
2025-01-14 16:33:43 +0100

    format code

--- generate-pdf.js
+++ generate-pdf.js
@@ -1,32 +1,32 @@
 import puppeteer from "puppeteer";
 import * as path from "path";
-import resume from "./resume.json" with { type: "json" };
+import resume from "./resume.json" with {type: "json"};
 
 (async () => {
-  if(!resume.basics.hasOwnProperty('pdfName')) {
-    console.info('resume.basics.pdfName not configured');
-    process.exit();
-  }
+    if (!resume.basics.hasOwnProperty('pdfName')) {
+        console.info('resume.basics.pdfName not configured');
+        process.exit();
+    }
 
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
-  const page = await browser.newPage();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    const page = await browser.newPage();
 
-  await page.goto(`file:${path.resolve('dist/index.html')}`);
+    await page.goto(`file:${path.resolve('dist/index.html')}`);
 
-  // Generate PDF
-  await page.pdf({
-    path: `dist/${resume.basics.pdfName}`,
-    format: 'A4',
-    printBackground: true,
-    displayHeaderFooter: false,
-    margin: {
-      top: '10mm',
-      bottom: '10mm',
-    },
-    scale: 0.8
-  });
+    // Generate PDF
+    await page.pdf({
+        path: `dist/${resume.basics.pdfName}`,
+        format: 'A4',
+        printBackground: true,
+        displayHeaderFooter: false,
+        margin: {
+            top: '10mm',
+            bottom: '10mm',
+        },
+        scale: 0.8
+    });
 
-  await browser.close();
+    await browser.close();
 })();
